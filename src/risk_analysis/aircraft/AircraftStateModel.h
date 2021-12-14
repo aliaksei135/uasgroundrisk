a0e60e9f0cb163e5b812d6fa687f302a6f9a82e5
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2021-12-14 16:31:58 +0000

    Hardcode maths defines

--- src/risk_analysis/aircraft/AircraftStateModel.h
+++ src/risk_analysis/aircraft/AircraftStateModel.h
@@ -12,30 +12,34 @@
 
 using namespace Eigen;
 
-namespace ugr {
-namespace risk {
-/**
- * A class encapsulating the current state of an aircraft
- */
-class AircraftStateModel {
-public:
-  Vector3d position;
-  Vector3d velocity;
+namespace ugr
+{
+	namespace risk
+	{
+		/**
+		 * A class encapsulating the current state of an aircraft
+		 */
+		class AircraftStateModel
+		{
+		public:
+			Vector3d position;
+			Vector3d velocity;
 
-  /**
-   * Return the heading of the aircraft in degrees
-   * @return
-   */
-  double getHeading() const {
-    return fmod((180 / M_PI) * atan2(velocity(0), velocity(1)) + 360, 360);
-  }
+			/**
+			 * Return the heading of the aircraft in degrees
+			 * @return
+			 */
+			double getHeading() const
+			{
+				return fmod((180 / 3.14159265358979323846) * atan2(velocity(0), velocity(1)) + 360, 360);
+			}
 
-  /**
-   * Return the altitude of the aircraft in metres
-   * @return
-   */
-  double getAltitude() const { return position(2); }
-};
-} // namespace risk
+			/**
+			 * Return the altitude of the aircraft in metres
+			 * @return
+			 */
+			double getAltitude() const { return position(2); }
+		};
+	} // namespace risk
 } // namespace ugr
 #endif // UASGROUNDRISK_SRC_RISK_ANALYSIS_AIRCRAFT_AIRCRAFTSTATEMODEL_H_
