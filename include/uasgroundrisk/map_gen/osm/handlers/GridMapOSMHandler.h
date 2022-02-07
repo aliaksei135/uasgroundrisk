931e9951ecdee6f7c3d6e23d9a1903b37e7ea3c4
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-07 13:28:46 +0000

    Generify OSM handler

--- include/uasgroundrisk/map_gen/osm/handlers/GridMapOSMHandler.h
+++ include/uasgroundrisk/map_gen/osm/handlers/GridMapOSMHandler.h
@@ -17,53 +17,55 @@
 
 namespace ugr
 {
-	namespace mapping
-	{
-		namespace osm
-		{
-			struct OSMTag;
-			using namespace ugr::gridmap;
-			using namespace ugr::mapping;
+    namespace mapping
+    {
+        namespace osm
+        {
+            struct OSMTag;
+            using namespace ugr::gridmap;
+            using namespace ugr::mapping;
 
-			class GridMapOSMHandler : public osmium::handler::Handler
-			{
-			public:
-				/**
-		 * Construct an osmium handler that write geometries to a gridmap.
-		 *
-		 * @param gridMap the gridmap to write to
-		 * @param tagLayerMap a map of OSM tags to gridmap layer names.
-		 * 					  Tags can only map to a single layer name,
-		 * but multiple tags can map to a single layer name.
-		 * @param densityGeometryMap a map of GEOS polygons to population values.
-		 * Geometries must be in EPSG:4326 projection. This class assumes
-		 * responsibility for freeing the GEOS objects here.
-		 * @param densityTagMap a map of OSM tags to uniform densities in
-		 * correspondingly tagged areas. densityGeometryMap takes precedence over this
-		 * in setting the grid map value.
-		 * @param gridCRS the grid coordinate reference system with units in metres
-		 */
-				GridMapOSMHandler(GeospatialGridMap* gridMap,
-				                  std::map<OSMTag, std::string> tagLayerMap,
-				                  const std::map<GEOSGeometry*, GridMapDataType>& densityGeometryMap,
-				                  std::map<OSMTag, GridMapDataType> densityTagMap,
-				                  std::string gridCRS = "EPSG:3395");
-				~GridMapOSMHandler();
+            class GridMapOSMHandler : public osmium::handler::Handler
+            {
+            public:
+                /**
+                 * \brief Construct an osmium handler that write geometries to a gridmap.
+                 *
+                 * \param gridMap the gridmap to write to
+                 * \param tagLayerMap a map of OSM tags to gridmap layer names.
+                 * 					  Tags can only map to a single layer name,
+                 * but multiple tags can map to a single layer name.
+                 * \param densityGeometryMap a map of GEOS polygons to population values.
+                 * Geometries must be in EPSG:4326 projection. This class assumes
+                 * responsibility for freeing the GEOS objects here.
+                 * \param densityTagMap a map of OSM tags to uniform densities in
+                 * correspondingly tagged areas. densityGeometryMap takes precedence over this
+                 * in setting the grid map value.
+                 * \param gridCRS the grid coordinate reference system with units in metres
+                 */
+                GridMapOSMHandler(GeospatialGridMap* gridMap,
+                                  std::map<OSMTag, std::string> tagLayerMap,
+                                  const std::map<GEOSGeometry*, GridMapDataType>& densityGeometryMap,
+                                  std::map<OSMTag, GridMapDataType> densityTagMap,
+                                  std::string gridCRS = "EPSG:3395");
+                ~GridMapOSMHandler();
 
-				void way(const osmium::Way& way) const noexcept;
+                void way(const osmium::Way& way) const noexcept;
+                void setFallbackValue(const std::string& layerName, const Index& gridMapPoint,
+                                      const Matrix::Scalar& fallbackDensity) const;
 
-			protected:
-				PJ_CONTEXT* projCtx;
-				PJ* reproj;
+            protected:
+                PJ_CONTEXT* projCtx;
+                PJ* reproj;
 
-				GeospatialGridMap* gridMap;
-				std::map<OSMTag, std::string> tagLayerMap;
-				std::map<OSMTag, GridMapDataType> densityTagMap;
-				std::map<GEOSGeometry*, GridMapDataType> densityGeometryMap;
+                GeospatialGridMap* gridMap;
+                std::map<OSMTag, std::string> tagLayerMap;
+                std::map<OSMTag, GridMapDataType> densityTagMap;
+                std::map<GEOSGeometry*, GridMapDataType> densityGeometryMap;
 
-				std::string gridCRS;
-			};
-		}
-	}
+                std::string gridCRS;
+            };
+        }
+    }
 }
 #endif // UASGROUNDRISK_SRC_MAP_GEN_GRIDMAPOSMHANDLER_H_
