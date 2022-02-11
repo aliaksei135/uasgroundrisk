f6c36b77f0a88e32e908d3ebc5db6c2d74d15df6
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-11 19:59:43 +0000

    Add temporal population map

---
+++ include/uasgroundrisk/map_gen/TemporalPopulationMap.h
@@ -0,0 +1,48 @@
+#ifndef UGR_TEMPORALPOPULATIONMAP_H
+#define UGR_TEMPORALPOPULATIONMAP_H
+#include "uasgroundrisk/map_gen/PopulationMap.h"
+
+namespace ugr
+{
+    namespace mapping
+    {
+        class TemporalPopulationMap : public PopulationMap
+        {
+        public:
+            TemporalPopulationMap(const std::array<float, 4>& bounds, const int resolution);
+
+            ~TemporalPopulationMap();
+
+            // Delete this for now, as all OSM tags come from the NHAPS mapping
+            void addOSMLayer(const std::string& layerName,
+                             const std::vector<osm::OSMTag>& tags,
+                             float defaultValue) = delete;
+
+            void setHourOfDay(const short hourOfDay);
+
+            std::vector<double> calculateAreas(const std::vector<GEOSGeometry*>& geoms) const;
+
+            void intersectResidentialGeometries();
+
+            void setOSMGeometries();
+
+            void fillGridMapPoly(const std::string& layerName, const GEOSGeometry* geom,
+                                 const GridMapDataType& geomDensity);
+
+            void eval();
+
+        protected:
+            short hourOfDay = 5;
+            int totalPopulation;
+            GEOSContextHandle_t geosCtx;
+
+            std::vector<std::vector<float>> nhapsProps;
+            std::vector<GEOSGeometry*> boundedGeometries;
+            std::map<GEOSGeometry*, double> geomAreas;
+            std::map<GEOSGeometry*, GridMapDataType> activeGeomDensityMap;
+            std::map<osm::OSMTag, std::vector<GEOSGeometry*>> tagGeomMap;
+            std::map<osm::OSMTag, double> tagAreas;
+        };
+    }
+}
+#endif // UGR_TEMPORALPOPULATIONMAP_H
