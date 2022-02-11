ab98148558018c57bb5a193d85c0848dd43e63ca
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-11 22:57:40 +0000

    Fix data dir var use

--- test/DataIngestTests.cpp
+++ test/DataIngestTests.cpp
@@ -1,16 +1,26 @@
 #include <algorithm>
 #include <gtest/gtest.h>
 #include <proj.h>
-#include "../../src/map_gen/census/Ingest.h"
-#include "../../src/utils/GeometryProjectionUtils.h"
+#include "../src/map_gen/census/Ingest.h"
+#include "../src/utils/GeometryProjectionUtils.h"
 #include <vector>
 
-
-TEST(DataIngestTests, GeometryIngestTest)
+class DataIngestTests : public ::testing::Test
 {
-    CensusGeometryIngest geomIngest;
+public:
+    void SetUp() override
+    {
+        geosCtx = initGEOS_r(notice, log_and_exit);
+    }
+
+    GEOSContextHandle_t geosCtx;
+};
+
+TEST_F(DataIngestTests, GeometryIngestTest)
+{
+    CensusGeometryIngest geomIngest(geosCtx);
     // Get the GEOS geoms, these are in EPSG:27700 so need reprojecting
-    auto geoms = geomIngest.readFile(std::string(UGR_DATA_DIR) + "england_wa_2011_clipped.shp");
+    auto geoms = geomIngest.readFile(UGR_DATA_DIR "england_wa_2011_clipped.shp");
 
     const auto projObjs = ugr::util::makeProjObject("EPSG:27700", "EPSG:3395");
     PJ* reproj = std::get<0>(projObjs);
@@ -27,27 +37,27 @@ TEST(DataIngestTests, GeometryIngestTest)
     ASSERT_EQ(geoms.size(), 7707);
 }
 
-TEST(DataIngestTests, DensityIngestTest)
+TEST_F(DataIngestTests, DensityIngestTest)
 {
-    CensusDensityIngest densityIngest;
-    const auto densityMap = densityIngest.readFile(std::string(UGR_DATA_DIR) + "density.csv");
+    CensusDensityIngest densityIngest(geosCtx);
+    const auto densityMap = densityIngest.readFile(UGR_DATA_DIR "density.csv");
 
     ASSERT_EQ(densityMap.size(), 8570);
 }
 
-TEST(DataIngestTests, NHAPSIngestTest)
+TEST_F(DataIngestTests, NHAPSIngestTest)
 {
     CensusNHAPSIngest nhapsIngest;
-    const auto nhapsProps = nhapsIngest.readFile(std::string(UGR_DATA_DIR) + "nhaps.json");
+    const auto nhapsProps = nhapsIngest.readFile(UGR_DATA_DIR "nhaps.json");
 
     ASSERT_EQ(nhapsProps.size(), 24);
     ASSERT_FLOAT_EQ(nhapsProps[3][0], 0.97);
 }
 
-TEST(DataIngestTests, MergedIngestTest)
+TEST_F(DataIngestTests, MergedIngestTest)
 {
-    CensusIngest censusIngest;
-    const auto out = censusIngest.makePopulationDensityMap();
+    CensusIngest censusIngest(geosCtx);
+    const auto out = censusIngest.makePopulationDensityMap<double>();
 
     ASSERT_EQ(out.size(), 7689);
 }
