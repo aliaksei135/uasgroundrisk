84940445de220bc903e52bb8dbd79904e9d5f4a2
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-11 19:48:11 +0000

    Fix geospatial gridmap reverse projection

--- src/map_gen/GeospatialGridMap.cpp
+++ src/map_gen/GeospatialGridMap.cpp
@@ -12,99 +12,99 @@ using namespace ugr::gridmap;
 
 
 ugr::mapping::GeospatialGridMap::GeospatialGridMap(
-	const std::array<float, 4> bounds, const float resolution, const char* worldSrs,
-	const char* projectionSrs) : bounds(bounds), xyRes(resolution)
+    const std::array<float, 4> bounds, const float resolution, const char* worldSrs,
+    const char* projectionSrs) : bounds(bounds), xyRes(resolution)
 {
-	setBounds(bounds, resolution);
-	projCtx = proj_context_create();
+    setBounds(bounds, resolution);
+    projCtx = proj_context_create();
 #ifdef PROJ_DATA_PATH
-	const char* projDataPaths[1];
-	projDataPaths[0] = PROJ_DATA_PATH;
-	proj_context_set_search_paths(projCtx, 1, projDataPaths);
+    const char* projDataPaths[1];
+    projDataPaths[0] = PROJ_DATA_PATH;
+    proj_context_set_search_paths(projCtx, 1, projDataPaths);
 #endif
-	reproj = proj_create_crs_to_crs(projCtx, worldSrs, projectionSrs, nullptr);
+    reproj = proj_create_crs_to_crs(projCtx, worldSrs, projectionSrs, nullptr);
 }
 
 ugr::mapping::GeospatialGridMap::~GeospatialGridMap()
 {
-	// if (reproj != nullptr)
-	// {
-	// 	proj_destroy(reproj);
-	// 	reproj = nullptr;
-	// }
-	// if (projCtx != nullptr)
-	// {
-	// 	proj_context_destroy(projCtx);
-	// 	projCtx = nullptr;
-	// }
-	proj_cleanup();
+    // if (reproj != nullptr)
+    // {
+    // 	proj_destroy(reproj);
+    // 	reproj = nullptr;
+    // }
+    // if (projCtx != nullptr)
+    // {
+    // 	proj_context_destroy(projCtx);
+    // 	projCtx = nullptr;
+    // }
+    proj_cleanup();
 }
 
 GridMapDataType ugr::mapping::GeospatialGridMap::atPosition(const std::string& layerName, const double lon,
-															const double lat) const
+                                                            const double lat) const
 {
-	const auto localIdx = world2Local(lon, lat);
-	return layers.at(layerName)(localIdx[0], localIdx[1]);
+    const auto localIdx = world2Local(lon, lat);
+    return layers.at(layerName)(localIdx[0], localIdx[1]);
 }
 
 GridMapDataType& ugr::mapping::GeospatialGridMap::atPosition(const std::string& layerName, const double lon,
-															 const double lat)
+                                                             const double lat)
 {
-	const auto localIdx = world2Local(lon, lat);
-	return layers.at(layerName)(localIdx[0], localIdx[1]);
+    const auto localIdx = world2Local(lon, lat);
+    return layers.at(layerName)(localIdx[0], localIdx[1]);
 }
 
 GridMapDataType ugr::mapping::GeospatialGridMap::atPosition(const std::string& layerName, const Position& pos) const
 {
-	return atPosition(layerName, pos[0], pos[1]);
+    return atPosition(layerName, pos[0], pos[1]);
 }
 
 GridMapDataType& ugr::mapping::GeospatialGridMap::atPosition(const std::string& layerName, const Position& pos)
 {
-	return atPosition(layerName, pos[0], pos[1]);
+    return atPosition(layerName, pos[0], pos[1]);
 }
 
 ugr::gridmap::Index ugr::mapping::GeospatialGridMap::world2Local(const Position& worldCoord) const
 {
-	return world2Local(worldCoord[0], worldCoord[1]);
+    return world2Local(worldCoord[0], worldCoord[1]);
 }
 
 ugr::gridmap::Index ugr::mapping::GeospatialGridMap::world2Local(const double lon, const double lat) const
 {
-	const auto reprojCoord = proj_trans(reproj, PJ_FWD, {lat, lon});
-	return {
-		sizeX - ((reprojCoord.enu.n - projectionOrigin[1]) / xyRes), (reprojCoord.enu.e - projectionOrigin[0]) / xyRes
-	};
+    const auto reprojCoord = proj_trans(reproj, PJ_FWD, {lat, lon});
+    return {
+        sizeX - ((reprojCoord.enu.n - projectionOrigin[1]) / xyRes), (reprojCoord.enu.e - projectionOrigin[0]) / xyRes
+    };
 }
 
 Position ugr::mapping::GeospatialGridMap::local2World(const int x, const int y) const
 {
-	const auto reprojCoord = proj_trans(reproj, PJ_INV, {
-											static_cast<double>(x) * xyRes + projectionOrigin[0],
-											static_cast<double>(y) * xyRes + projectionOrigin[1]
-										});
-	return {sizeX - reprojCoord.enu.n, reprojCoord.enu.e};
+    const auto reprojCoord = proj_trans(reproj, PJ_INV, {
+                                            static_cast<double>(sizeX - x) * xyRes + projectionOrigin[0],
+                                            static_cast<double>(y) * xyRes + projectionOrigin[1]
+                                        });
+    return {reprojCoord.enu.n, reprojCoord.enu.e};
 }
 
 Position ugr::mapping::GeospatialGridMap::local2World(const gridmap::Index& localCoord) const
 {
-	return local2World(localCoord[0], localCoord[1]);
+    return local2World(localCoord[0], localCoord[1]);
 }
 
 void ugr::mapping::GeospatialGridMap::setBounds(
-	const std::array<float, 4> boundsArr, const float resolution)
+    const std::array<float, 4> boundsArr, const float resolution)
 {
-	// This reprojects EPSG:4326 to EPSG:3395 by default
-	const auto swProjPoint = util::reprojectCoordinate(boundsArr[0], boundsArr[1]);
-	this->projectionOrigin = {swProjPoint.enu.e, swProjPoint.enu.n, 0};
-	//TODO: Should an altitude be set in the projection origin?
-	const auto neProjPoint = util::reprojectCoordinate(boundsArr[2], boundsArr[3]);
-	const auto dx = std::abs(swProjPoint.enu.e - neProjPoint.enu.e);
-	const auto dy = std::abs(swProjPoint.enu.n - neProjPoint.enu.n);
-	const int xLength = static_cast<int>(dx / static_cast<float>(resolution));
-	const int yLength = static_cast<int>(dy / static_cast<float>(resolution));
+    // This reprojects EPSG:4326 to EPSG:3395 by default
+    const auto swProjPoint = util::reprojectCoordinate(boundsArr[0], boundsArr[1]);
+    this->projectionOrigin = {swProjPoint.enu.e, swProjPoint.enu.n, 0};
+    //TODO: Should an altitude be set in the projection origin?
+    const auto neProjPoint = util::reprojectCoordinate(boundsArr[2], boundsArr[3]);
+    const auto dx = std::abs(swProjPoint.enu.e - neProjPoint.enu.e);
+    const auto dy = std::abs(swProjPoint.enu.n - neProjPoint.enu.n);
+    const int xLength = static_cast<int>(dx / static_cast<float>(resolution));
+    const int yLength = static_cast<int>(dy / static_cast<float>(resolution));
 
-	// x,y are swapped here to match the expected orientation of the matrix if plotted.
-	// Eigen uses row,column indexing, which would map to lat, lon not the other way around
-	setGeometry(yLength, xLength);
+    // x,y are swapped here to match the expected orientation of the matrix if plotted.
+    // Eigen uses row,column indexing, which would map to lat, lon not the other way around
+    setGeometry(yLength, xLength);
 }
