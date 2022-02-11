7f8097e41068d64e3ce5063db69c172886cef399
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-11 00:41:49 +0000

    Fix layer constant value setting

--- src/gridmap/GridMap.cpp
+++ src/gridmap/GridMap.cpp
@@ -5,100 +5,100 @@ using namespace ugr::gridmap;
 
 Vector2i GridMap::getSize() const
 {
-	return {sizeX, sizeY};
+    return {sizeX, sizeY};
 }
 
 void GridMap::setGeometry(const int sizeX, const int sizeY)
 {
-	this->sizeX = sizeX;
-	this->sizeY = sizeY;
-	geometrySet = true;
+    this->sizeX = sizeX;
+    this->sizeY = sizeY;
+    geometrySet = true;
 }
 
 std::vector<std::string> GridMap::getLayers() const
 {
-	std::vector<std::string> keys;
-	keys.reserve(layers.size());
-	for (const auto& pair : layers)
-	{
-		keys.emplace_back(pair.first);
-	}
-	return keys;
+    std::vector<std::string> keys;
+    keys.reserve(layers.size());
+    for (const auto& pair : layers)
+    {
+        keys.emplace_back(pair.first);
+    }
+    return keys;
 }
 
 void GridMap::add(const std::string& layerName, const Matrix& layerData)
 {
-	if (geometrySet)
-	{
-		layers.insert({layerName, layerData});
-	}
-	else
-	{
-		throw std::out_of_range("GridMap Geometry not set");
-	}
+    if (geometrySet)
+    {
+        layers.insert({layerName, layerData});
+    }
+    else
+    {
+        throw std::out_of_range("GridMap Geometry not set");
+    }
 }
 
 void GridMap::add(const std::string& layerName, const double constValue)
 {
-	if (geometrySet)
-	{
-		Matrix layerData(sizeX, sizeY);
-		layerData.setConstant(constValue);
-		layers.emplace(layerName, layerData);
-	}
-	else
-	{
-		throw std::out_of_range("GridMap Geometry not set");
-	}
+    if (geometrySet)
+    {
+        Matrix layerData(sizeX, sizeY);
+        layerData.setConstant(constValue);
+        layers.emplace(layerName, layerData);
+    }
+    else
+    {
+        throw std::out_of_range("GridMap Geometry not set");
+    }
 }
 
 ::Matrix GridMap::get(const std::string& layerName) const
 {
-	return layers.at(layerName);
+    return layers.at(layerName);
 }
 
 ::Matrix& GridMap::get(const std::string& layerName)
 {
-	return layers.at(layerName);
+    return layers.at(layerName);
 }
 
 const ::Matrix& GridMap::operator[](const std::string& layerName) const
 {
-	return layers.at(layerName);
+    return layers.at(layerName);
 }
 
 ::Matrix& GridMap::operator[](const std::string& layerName)
 {
-	return layers.at(layerName);
+    return layers.at(layerName);
 }
 
 GridMapDataType GridMap::at(const std::string& layerName, const int i, const int j) const
 {
-	return layers.at(layerName)(i, j);
+    return layers.at(layerName)(i, j);
 }
 
 GridMapDataType& GridMap::at(const std::string& layerName, const int i, const int j)
 {
-	return layers.at(layerName)(i, j);
+    return layers.at(layerName)(i, j);
 }
 
 GridMapDataType GridMap::at(const std::string& layerName, const Index& idx) const
 {
-	return layers.at(layerName)(idx(0), idx(1));
+    return layers.at(layerName)(idx(0), idx(1));
 }
 
 GridMapDataType& GridMap::at(const std::string& layerName, const Index& idx)
 {
-	return layers.at(layerName)(idx(0), idx(1));
+    return layers.at(layerName)(idx(0), idx(1));
 }
 
 bool GridMap::isInBounds(const Index& localCoord) const
 {
-	if (geometrySet)
-	{
-		return localCoord(0) >= 0 && localCoord(1) >= 0 && localCoord(0) < sizeX && localCoord(1) < sizeY;
-	}
-	throw std::out_of_range("GridMap Geometry not set");
+    if (geometrySet)
+    {
+        return localCoord(0) >= 0 && localCoord(1) >= 0 && localCoord(0) < sizeX && localCoord(1) < sizeY;
+    }
+    throw std::out_of_range("GridMap Geometry not set");
 }
 
 void GridMap::writeToNetCDF(const std::string& path) const
