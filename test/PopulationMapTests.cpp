0d7be2b81e483997cb73496a9a75af5b1bd99bfa
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-12 00:53:51 +0000

    Bump layer numbers for building height layer in tests

--- test/PopulationMapTests.cpp
+++ test/PopulationMapTests.cpp
@@ -22,78 +22,78 @@ using namespace ugr::gridmap;
 class PopulationMapTests : public ::testing::Test
 {
 protected:
-	std::array<float, 4> bounds{
-		50.9065510f, -1.4500237f, 50.9517765f,
-		-1.3419628f
-	};
-	int resolution = 20;
+    std::array<float, 4> bounds{
+        50.9065510f, -1.4500237f, 50.9517765f,
+        -1.3419628f
+    };
+    int resolution = 20;
 };
 
 TEST_F(PopulationMapTests, EmptyMapTest)
 {
-	PopulationMap popMap(bounds, resolution);
-	popMap.eval();
+    PopulationMap popMap(bounds, resolution);
+    popMap.eval();
 
-	ASSERT_EQ(popMap.getLayers().size(), 1);
+    ASSERT_EQ(popMap.getLayers().size(), 2);
 
-	auto size = popMap.getSize();
-	ASSERT_EQ(size.y(), 601);
-	ASSERT_EQ(size.x(), 398);
+    auto size = popMap.getSize();
+    ASSERT_EQ(size.y(), 601);
+    ASSERT_EQ(size.x(), 398);
 
-	const Position outPos(-1.338997, 50.933289);
-	ASSERT_FALSE(popMap.isInBounds(popMap.world2Local(outPos)));
+    const Position outPos(-1.338997, 50.933289);
+    ASSERT_FALSE(popMap.isInBounds(popMap.world2Local(outPos)));
 
-	const Position inPos(-1.404258, 50.922982);
-	ASSERT_TRUE(popMap.isInBounds(popMap.world2Local(inPos)));
+    const Position inPos(-1.404258, 50.922982);
+    ASSERT_TRUE(popMap.isInBounds(popMap.world2Local(inPos)));
 }
 
 TEST_F(PopulationMapTests, SingleLayerTest)
 {
-	PopulationMap popMap(bounds, resolution);
-	popMap.addOSMLayer("Schools", {OSMTag("amenity", "school")}, 10);
-	popMap.eval();
+    PopulationMap popMap(bounds, resolution);
+    popMap.addOSMLayer("Schools", {OSMTag("amenity", "school")}, 10);
+    popMap.eval();
 
-	// Inside Bitterne Park School, Southampton
-	const Position testPos(-1.369220, 50.929116);
+    // Inside Bitterne Park School, Southampton
+    const Position testPos(-1.369220, 50.929116);
 
-	ASSERT_EQ(popMap.getLayers().size(), 2);
-	ASSERT_EQ(popMap.get("Schools").maxCoeff(), 10);
-	EXPECT_EQ(popMap.atPosition("Schools", testPos), 10);
+    ASSERT_EQ(popMap.getLayers().size(), 3);
+    ASSERT_EQ(popMap.get("Schools").maxCoeff(), 10);
+    EXPECT_EQ(popMap.atPosition("Schools", testPos), 10);
 
-	auto size = popMap.getSize();
+    auto size = popMap.getSize();
 
-	const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+    const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
 
-	std::ofstream file("schools_pop_mat.csv");
-	if (file.is_open())
-	{
-		file << popMap.get("Schools").format(CSVFormat);
-		file.close();
-	}
+    std::ofstream file("schools_pop_mat.csv");
+    if (file.is_open())
+    {
+        file << popMap.get("Schools").format(CSVFormat);
+        file.close();
+    }
 
 	outputMat(popMap.get("Schools"), ::testing::UnitTest::GetInstance()->current_test_info()->name());
 }
 
 TEST_F(PopulationMapTests, MultiLayerTest)
 {
-	PopulationMap popMap(bounds, resolution);
-	popMap.addOSMLayer("Schools", {{"amenity", "school"}}, 10);
-	popMap.addOSMLayer("Retail", {{"landuse", "retail"}}, 20);
-	popMap.eval();
+    PopulationMap popMap(bounds, resolution);
+    popMap.addOSMLayer("Schools", {{"amenity", "school"}}, 10);
+    popMap.addOSMLayer("Retail", {{"landuse", "retail"}}, 20);
+    popMap.eval();
 
-	ASSERT_EQ(popMap.getLayers().size(), 3);
+    ASSERT_EQ(popMap.getLayers().size(), 4);
 
-	// Inside Bitterne Park School, Southampton
-	const Position testSchoolPos(-1.369220, 50.929116);
+    // Inside Bitterne Park School, Southampton
+    const Position testSchoolPos(-1.369220, 50.929116);
 
-	ASSERT_EQ(popMap.get("Schools").maxCoeff(), 10);
-	EXPECT_EQ(popMap.atPosition("Schools", testSchoolPos), 10);
+    ASSERT_EQ(popMap.get("Schools").maxCoeff(), 10);
+    EXPECT_EQ(popMap.atPosition("Schools", testSchoolPos), 10);
 
-	// Portswood shops, Southampton
-	const Position testRetailPos(-1.393254, 50.925358);
+    // Portswood shops, Southampton
+    const Position testRetailPos(-1.393254, 50.925358);
 
-	ASSERT_EQ(popMap.get("Retail").maxCoeff(), 20);
-	EXPECT_EQ(popMap.atPosition("Retail", testRetailPos), 20);
+    ASSERT_EQ(popMap.get("Retail").maxCoeff(), 20);
+    EXPECT_EQ(popMap.atPosition("Retail", testRetailPos), 20);
 
 	outputMat(popMap.get("Population Density"), ::testing::UnitTest::GetInstance()->current_test_info()->name());
 
@@ -101,6 +101,6 @@ TEST_F(PopulationMapTests, MultiLayerTest)
 
 int main(int argc, char** argv)
 {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
