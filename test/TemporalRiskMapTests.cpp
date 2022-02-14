3f9d8011d8d7198aa941432de228acfe87fcc8b9
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-14 08:42:02 +0000

    Don't use make_unique in tests

--- test/TemporalRiskMapTests.cpp
+++ test/TemporalRiskMapTests.cpp
@@ -24,11 +24,11 @@ protected:
         aircraft.width = 5;
 
         aircraft.descents.emplace_back(
-            std::make_unique<GlideDescentModel>(90, 2.8, 3.2, 21, 15));
+            std::unique_ptr<GlideDescentModel>(new GlideDescentModel(90, 2.8, 3.2, 21, 15)));
         aircraft.descents.emplace_back(
-            std::make_unique<BallisticDescentModel>(90, 2.8, 3.2, 0.6 * 0.6, 0.8));
+            std::unique_ptr<BallisticDescentModel>(new BallisticDescentModel(90, 2.8, 3.2, 0.6 * 0.6, 0.8)));
         // aircraft.descents.emplace_back(
-        //     std::make_unique<ParachuteDescentModel>(90, 2.8, 3.2, 1.2, 12.5, 2));
+        //     std::unique_ptr<ParachuteDescentModel>(new ParachuteDescentModel(90, 2.8, 3.2, 1.2, 12.5, 2)));
     }
 
     std::array<float, 4> bounds{
