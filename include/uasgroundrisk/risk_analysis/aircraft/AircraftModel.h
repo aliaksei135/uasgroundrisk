dd5364c013ad6181127992b14c1834c91ac7d0b5
Aliaksei Pilko <A.Pilko@soton.ac.uk>
2022-02-15 12:18:42 +0000

    Enforce matching physical params for descent models creation through forwarding

--- include/uasgroundrisk/risk_analysis/aircraft/AircraftModel.h
+++ include/uasgroundrisk/risk_analysis/aircraft/AircraftModel.h
@@ -8,29 +8,38 @@
 
 namespace ugr
 {
-	namespace risk
-	{
-		class AircraftModel
-		{
-		public:
-			AircraftModel() = default;
+    namespace risk
+    {
+        class AircraftModel
+        {
+        public:
+            AircraftModel() = default;
 
-			AircraftModel(const double mass, const double width, const double length): mass(mass), width(width),
-				length(length)
-			{
-			}
+            AircraftModel(const double mass, const double width, const double length): mass(mass), width(width),
+                length(length)
+            {
+            }
 
-			AircraftModel(const AircraftModel& other) = delete;
-			AircraftModel(AircraftModel&& other) noexcept = default;
-			AircraftModel& operator=(const AircraftModel& other) = delete;
-			AircraftModel& operator=(AircraftModel&& other) noexcept = default;
+            AircraftModel(const AircraftModel& other) = delete;
+            AircraftModel(AircraftModel&& other) noexcept = default;
+            AircraftModel& operator=(const AircraftModel& other) = delete;
+            AircraftModel& operator=(AircraftModel&& other) noexcept = default;
 
-			AircraftStateModel state;
-			std::vector<std::unique_ptr<DescentModel>> descents;
-			double mass;
-			double width;
-			double length;
-		};
-	}
+            template <typename DescentModel, typename...Args>
+            void addDescentModel(Args&&... modelArgs)
+            {
+                descents.emplace_back(
+                    std::unique_ptr<DescentModel>(
+                        new DescentModel(mass, width, length, std::forward<Args>(modelArgs)...))
+                );
+            }
+
+            AircraftStateModel state;
+            double mass;
+            double width;
+            double length;
+            std::vector<std::unique_ptr<DescentModel>> descents;
+        };
+    }
 }
 #endif // AIRCRAFTMODEL_H
