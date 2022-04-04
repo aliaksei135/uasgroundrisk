873ebf5e744ba68d748b5da540a7754c8de714ce
Aliaksei Pilko <aliakseipilko1@gmail.com>
2022-04-04 15:01:08 +0100

    Modify aircraft model for pybind compatibility

--- include/uasgroundrisk/risk_analysis/aircraft/AircraftModel.h
+++ include/uasgroundrisk/risk_analysis/aircraft/AircraftModel.h
@@ -8,38 +8,32 @@
 
 namespace ugr
 {
-    namespace risk
-    {
-        class AircraftModel
-        {
-        public:
-            AircraftModel() = default;
+	namespace risk
+	{
+		class AircraftModel
+		{
+		public:
+			AircraftModel() = default;
 
-            AircraftModel(const double mass, const double width, const double length): mass(mass), width(width),
-                length(length)
-            {
-            }
+			AircraftModel(const double mass, const double width, const double length): mass(mass), width(width),
+				length(length)
+			{
+			}
 
-            AircraftModel(const AircraftModel& other) = delete;
-            AircraftModel(AircraftModel&& other) noexcept = default;
-            AircraftModel& operator=(const AircraftModel& other) = delete;
-            AircraftModel& operator=(AircraftModel&& other) noexcept = default;
+			template <typename DescentModel, typename...Args>
+			void addDescentModel(Args&&... modelArgs)
+			{
+				descents.emplace_back(
+					std::make_shared<DescentModel>(mass, width, length, std::forward<Args>(modelArgs)...)
+				);
+			}
 
-            template <typename DescentModel, typename...Args>
-            void addDescentModel(Args&&... modelArgs)
-            {
-                descents.emplace_back(
-                    std::unique_ptr<DescentModel>(
-                        new DescentModel(mass, width, length, std::forward<Args>(modelArgs)...))
-                );
-            }
-
-            AircraftStateModel state;
-            double mass;
-            double width;
-            double length;
-            std::vector<std::unique_ptr<DescentModel>> descents;
-        };
-    }
+			AircraftStateModel state;
+			double mass;
+			double width;
+			double length;
+			std::vector<std::shared_ptr<DescentModel>> descents;
+		};
+	}
 }
 #endif // AIRCRAFTMODEL_H
